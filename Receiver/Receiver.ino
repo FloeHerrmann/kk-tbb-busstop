ada681bf22e018a2d8f0394ca5b23da71a231eea
Florian Herrmann <florian.herrmann@m2m-systems.de>
2015-05-12 12:58:57 +0200

    Bidirektionale Kommunikation
    Projekt um bidirektionale Kommunikation und das ansteuern von LEDs erweitert

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -3,84 +3,56 @@
 #define DEVICEID "RE"
 //#define DEVICEID "RE"
 
+#define RADIO 8
+#define LED_INT 13
+#define LED_GREEN 2
+
 void setup() {
 	Serial.begin(115200);
         Serial.println( "STARTED" );
 
-        pinMode( 13 , OUTPUT );
-        pinMode( 2 , OUTPUT );
-        pinMode( 3 , OUTPUT );
-        pinMode( 4 , OUTPUT );
-        pinMode( 8 , OUTPUT );
+        pinMode( LED_INT , OUTPUT );
+        pinMode( LED_GREEN , OUTPUT );
+        pinMode( RADIO , OUTPUT );
         
-        digitalWrite( 2 , HIGH );
-        digitalWrite( 3 , HIGH );
-        digitalWrite( 4 , HIGH );
+        digitalWrite( LED_GREEN , HIGH );
         
-        digitalWrite( 8 , HIGH );
+        digitalWrite( RADIO , HIGH );
         
         LLAP.init( );
       
-        digitalWrite( 13 , HIGH );
-        delay( 266 );
-        digitalWrite( 13 , LOW );
-        delay( 100 );
-        
-        digitalWrite( 13 , HIGH );
-        delay( 266 );
-        digitalWrite( 13 , LOW );
-        delay( 100 );
-        
-        digitalWrite( 13 , HIGH );
-        delay( 266 );
-        digitalWrite( 13 , LOW );
+        startUpSignal();
 }
 
 void loop() {
   
 	if (LLAP.bMsgReceived) {
-	    Serial.print("Message is:");
-            Serial.println(LLAP.sMessage);
-            if( LLAP.sMessage.equals( "LEDRE----" ) ) {
-              digitalWrite( 2 , LOW );
-              digitalWrite( 3 , HIGH );
-              digitalWrite( 4 , HIGH );
-            } else if( LLAP.sMessage.equals( "LEDGR----" ) ) {
-              digitalWrite( 2 , HIGH );
-              digitalWrite( 3 , LOW );
-              digitalWrite( 4 , HIGH );
-            } else if( LLAP.sMessage.equals( "LEDBL----" ) ) {
-              digitalWrite( 2 , HIGH );
-              digitalWrite( 3 , HIGH );
-              digitalWrite( 4 , LOW );
-            } else if( LLAP.sMessage.equals( "LEDREGR--" ) ) {
-              digitalWrite( 2 , LOW );
-              digitalWrite( 3 , LOW );
-              digitalWrite( 4 , HIGH );
-            } else if( LLAP.sMessage.equals( "LEDREBL--" ) ) {
-              digitalWrite( 2 , LOW );
-              digitalWrite( 3 , HIGH );
-              digitalWrite( 4 , LOW );
-            } else if( LLAP.sMessage.equals( "LEDGRBL--" ) ) {
-              digitalWrite( 2 , HIGH );
-              digitalWrite( 3 , LOW );
-              digitalWrite( 4 , LOW );
-            } else if( LLAP.sMessage.equals( "LEDREGRBL" ) ) {
-              digitalWrite( 2 , LOW );
-              digitalWrite( 3 , LOW );
-              digitalWrite( 4 , LOW );
-            } else if( LLAP.sMessage.equals( "LEDOFF---" ) ) {
-              digitalWrite( 2 , HIGH );
-              digitalWrite( 3 , HIGH );
-              digitalWrite( 4 , HIGH );
+	    //Serial.print("Message is:");
+            //Serial.println(LLAP.sMessage);
+            if( LLAP.sMessage.equals( "BUSTBB---" ) ) {
+              digitalWrite( LED_GREEN , LOW );
+              LLAP.sendMessage( F("TBBONOK") );
+            } else if( LLAP.sMessage.equals( "---------" ) ) {
+              digitalWrite( LED_GREEN , HIGH );
+              LLAP.sendMessage( F("TBBOFFOK") );
             }
             LLAP.bMsgReceived = false;
 	}
-	/*
-	static unsigned long lastTime = millis();
-	if (millis() - lastTime >= 5000) {
-		lastTime = millis();
-		LLAP.sendMessage(F("HELLO"));
-	}
-        */
+
+}
+
+void startUpSignal() {
+  digitalWrite( LED_INT , HIGH );
+  delay( 266 );
+  digitalWrite( LED_INT , LOW );
+  delay( 100 );
+        
+  digitalWrite( LED_INT , HIGH );
+  delay( 266 );
+  digitalWrite( LED_INT , LOW );
+  delay( 100 );
+        
+  digitalWrite( LED_INT , HIGH );
+  delay( 266 );
+  digitalWrite( LED_INT , LOW );
 }
--- Remote/Remote.ino
+++ Remote/Remote.ino
@@ -1,62 +1,124 @@
 #include "LLAPSerial.h"	// include the library
 
-#define DEVICEID "MA"
-//#define DEVICEID "RE"
+#define BUTTON 7
+#define RADIO 8
+#define LED_INT 13
+#define LED_RED 2
+#define LED_GREEN 3
+#define LED_BLUE 4
 
-byte step = -1;
+#define ANSWER_TIMEOUT 10000
+
+bool isOn = false;
 bool send = false;
+bool waitForAnswer = false;
+unsigned long answerTimer = 0;
 
 void setup() {
+	
 	Serial.begin( 115200 );
-        Serial.println( "RUNNING" );
-        
-        pinMode( 13 , OUTPUT );
-        digitalWrite( 13 , LOW );
-        
-        pinMode( 2 , INPUT_PULLUP );
-        
-	pinMode( 8 , OUTPUT );
-	digitalWrite( 8 , HIGH );
+	Serial.println( "RUNNING" );
+			
+	pinMode( LED_INT , OUTPUT );		
+	pinMode( LED_RED , OUTPUT );        
+	pinMode( LED_GREEN , OUTPUT );
+	pinMode( LED_BLUE , OUTPUT );
+	pinMode( BUTTON , INPUT_PULLUP );
+	pinMode( RADIO , OUTPUT );
 
+	LED( false , false , false );
+	digitalWrite( RADIO , HIGH );
 	LLAP.init( );
 
-	//LLAP.sendMessage( F( "STARTED" ) );
+	startUpSignal();
+
 }
 
 void loop() {
-  
-  
-  if( digitalRead( 2 ) == 0 ) {
-    step++;
-    delay( 250 );
-    send = true;
-  }
-  
-  if( step == 0 && send == true ) {
-    LLAP.sendMessage(F("LEDRE"));
-    send = false;delay( 1000 );
-  } else if( step == 1 && send == true ) {
-    LLAP.sendMessage(F("LEDGR"));
-    send = false;delay( 1000 );
-  } else if( step == 2 && send == true ) {
-    LLAP.sendMessage(F("LEDBL"));
-    send = false;delay( 1000 );
-  } else if( step == 3 && send == true ) {
-    LLAP.sendMessage(F("LEDREGR"));
-    send = false;delay( 1000 );
-  } else if( step == 4 && send == true ) {
-    LLAP.sendMessage(F("LEDREBL"));
-    send = false;delay( 1000 );
-  } else if( step == 5 && send == true ) {
-    LLAP.sendMessage(F("LEDGRBL"));
-    send = false;delay( 1000 );
-  } else if( step == 6 && send == true ) {
-    LLAP.sendMessage(F("LEDREGRBL"));
-    send = false;delay( 1000 );
-  }  else if( step == 7 && send == true ) {
-    LLAP.sendMessage(F("LEDOFF"));
-    send = false;delay( 1000 );
-    step = -1;
-  }
+	
+	if( waitForAnswer == true && send == false ) {
+		if (LLAP.bMsgReceived) {
+			if( LLAP.sMessage.equals( "TBBONOK--" ) ) {
+				LED( false , true , false );
+				waitForAnswer = false;
+			} else if( LLAP.sMessage.equals( "TBBOFFOK-" ) ) {
+				LED( false , true , false );
+				delay( 1000 );
+				waitForAnswer = false;
+				LED( false , false , false );
+			}
+			LLAP.bMsgReceived = false;
+		} else {
+			if( (millis() - answerTimer) > ANSWER_TIMEOUT ) {
+				LEDError();
+				waitForAnswer = false;
+			}
+		}
+	} else {
+		if( digitalRead( BUTTON ) == 0 ) {
+			isOn = !isOn;
+			send = true;
+			delay( 200 );
+		}
+		 
+		if( isOn == true && send == true ) {
+			LLAP.sendMessage(F("BUSTBB"));
+			LED( false , false , true );
+			send = false;
+			waitForAnswer = true;
+			answerTimer = millis();
+			delay( 500 );
+		} else if( isOn == false && send == true ) {
+			LLAP.sendMessage(F("------"));
+			LED( false , false , false );
+			send = false;
+			waitForAnswer = true;
+			answerTimer = millis();
+			delay( 500 );
+		}
+	}
+}
+
+void startUpSignal() {
+	digitalWrite( LED_INT , HIGH );
+	delay( 266 );
+	digitalWrite( LED_INT , LOW );
+	delay( 100 );
+			
+	digitalWrite( LED_INT , HIGH );
+	delay( 266 );
+	digitalWrite( LED_INT , LOW );
+	delay( 100 );
+			
+	digitalWrite( LED_INT , HIGH );
+	delay( 266 );
+	digitalWrite( LED_INT , LOW );
+}
+
+void LED( bool red , bool green , bool blue ) {
+	if( red == true ) digitalWrite( LED_RED , LOW );
+	else digitalWrite( LED_RED , HIGH );
+	
+	if( green == true ) digitalWrite( LED_GREEN , LOW );
+	else digitalWrite( LED_GREEN , HIGH );
+	
+	if( blue == true ) digitalWrite( LED_BLUE , LOW );
+	else digitalWrite( LED_BLUE , HIGH );
+}
+
+void LEDError() {
+	LED( true , false , false );
+	delay( 1000 );
+	LED( false , false , false );
+	delay( 1000 );
+
+	LED( true , false , false );
+	delay( 1000 );
+	LED( false , false , false );
+	delay( 1000 );
+
+	LED( true , false , false );
+	delay( 1000 );
+	LED( false , false , false );
 
 }
